331ae0d739a4af04e5d28132631f344b84de4ede
DJ <darylcuenco@yahoo.com>
2023-04-04 22:53:19 +0800

    place connection string inside config file

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,12 @@ import { DbmMongoController } from './controllers/dbm-mongo/dbm-mongo.controller
 import { DbmMongoService } from './services/dbm-mongo/dbm-mongo.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { DBSchema } from './models/dbm-model/dbm-model';
+import { Config } from './config/config';
+
+const config : Config = new Config();
 @Module({
   imports: [MongooseModule
-    .forRoot('mongodb+srv://admin:D3SEx5kCudrOWDlR@dbmanager.hszctwp.mongodb.net/?retryWrites=true&w=majority'),
+    .forRoot(config.connStr),
     MongooseModule.forFeature([{name:'DBManager',schema: DBSchema}])
   ],
   controllers: [DbmMongoController],
---
+++ src/config/config.ts
@@ -0,0 +1,5 @@
+export class Config {
+    constructor(
+        public connStr: string = 'mongodb+srv://admin:D3SEx5kCudrOWDlR@dbmanager.hszctwp.mongodb.net/?retryWrites=true&w=majority'
+      ) {}
+}
